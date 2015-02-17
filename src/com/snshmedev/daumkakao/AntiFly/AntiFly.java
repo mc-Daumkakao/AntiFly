248bde31dbfa118b7ab6c8123f0b021bc3d7a29d
mc-Daumkakao <antimbgh@naver.com>
2015-02-18 00:11:44 +0900

    일반유저 접근제한

--- src/com/snshmedev/daumkakao/AntiFly/AntiFly.java
+++ src/com/snshmedev/daumkakao/AntiFly/AntiFly.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -25,10 +26,17 @@ public class AntiFly extends JavaPlugin{
 		}
 		//커맨드
 		public boolean onCommand(CommandSender sender, Command cmd, String Label,String[]args) {
-		  sender.sendMessage(ChatColor.AQUA + "/af add" + " - " + ChatColor.YELLOW + "해당 월드의 플라이를 차단합니다.");
-		  sender.sendMessage(ChatColor.AQUA + "/af remove" + " - " + ChatColor.YELLOW + "해당 월드의 플라이 차단을 해제합니다.");
-		  sender.sendMessage(ChatColor.AQUA + "/af list" + " - " + ChatColor.YELLOW + "플라이가 차단된 월드의 목록을 불러옵니다.");
-		  sender.sendMessage(ChatColor.AQUA + "/af reload" + " - " + ChatColor.YELLOW + "config의 월드목록을 리로딩합니다.");
-		  return true;
+			Player p = (Player) sender;
+			if(p.isOp()){
+				  sender.sendMessage(ChatColor.AQUA + "/af add" + " - " + ChatColor.YELLOW + "해당 월드의 플라이를 차단합니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af remove" + " - " + ChatColor.YELLOW + "해당 월드의 플라이 차단을 해제합니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af list" + " - " + ChatColor.YELLOW + "플라이가 차단된 월드의 목록을 불러옵니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af reload" + " - " + ChatColor.YELLOW + "config의 월드목록을 리로딩합니다.");
+				  return true;
+			}else{
+				sender.sendMessage(ChatColor.RED + "당신은 권한이 없습니다.");
+				return true;
+			}
+
 		 }
 }
