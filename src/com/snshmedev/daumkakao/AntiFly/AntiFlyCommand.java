cad047b5f48067d95a4d31b0b39da207dc850ba2
mc-Daumkakao <antimbgh@naver.com>
2015-02-18 21:23:30 +0900

    커맨드 구체화

--- src/com/snshmedev/daumkakao/AntiFly/AntiFlyCommand.java
+++ src/com/snshmedev/daumkakao/AntiFly/AntiFlyCommand.java
@@ -12,15 +12,33 @@ public class AntiFlyCommand implements CommandExecutor{
 	public boolean onCommand(CommandSender sender, Command cmd, String Label,String[]args) {
 		Player p = (Player) sender;
 		if(p.isOp()){
-			  sender.sendMessage(ChatColor.AQUA + "/af add <월드이름>" + " - " + ChatColor.YELLOW + "해당 월드의 플라이를 차단합니다.");
-			  sender.sendMessage(ChatColor.AQUA + "/af remove <월드이름>" + " - " + ChatColor.YELLOW + "해당 월드의 플라이 차단을 해제합니다.");
-			  sender.sendMessage(ChatColor.AQUA + "/af list" + " - " + ChatColor.YELLOW + "플라이가 차단된 월드의 목록을 불러옵니다.");
-			  sender.sendMessage(ChatColor.AQUA + "/af reload" + " - " + ChatColor.YELLOW + "config의 월드목록을 리로딩합니다.");
-			  return true;
+			if(0<args.length){
+				if(args[0].equalsIgnoreCase("add")){
+					sender.sendMessage("/af add");
+				}else if(args[0].equalsIgnoreCase("remove")){
+					sender.sendMessage("/af remove");
+				}else if(args[0].equalsIgnoreCase("list")){
+					sender.sendMessage("/af list");
+				}else if(args[0].equalsIgnoreCase("reload")){
+					sender.sendMessage("/af reload");
+				}else if(args[0].equalsIgnoreCase("help")){
+				  sender.sendMessage(ChatColor.AQUA + "/af add <월드이름>" + " - " + ChatColor.YELLOW + "해당 월드의 플라이를 차단합니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af remove <월드이름>" + " - " + ChatColor.YELLOW + "해당 월드의 플라이 차단을 해제합니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af list" + " - " + ChatColor.YELLOW + "플라이가 차단된 월드의 목록을 불러옵니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af reload" + " - " + ChatColor.YELLOW + "config의 월드목록을 리로딩합니다.");
+				  return true;
+				}else{
+				  sender.sendMessage(ChatColor.AQUA + "/af add <월드이름>" + " - " + ChatColor.YELLOW + "해당 월드의 플라이를 차단합니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af remove <월드이름>" + " - " + ChatColor.YELLOW + "해당 월드의 플라이 차단을 해제합니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af list" + " - " + ChatColor.YELLOW + "플라이가 차단된 월드의 목록을 불러옵니다.");
+				  sender.sendMessage(ChatColor.AQUA + "/af reload" + " - " + ChatColor.YELLOW + "config의 월드목록을 리로딩합니다.");
+				  return true;
+				}
+			}
 		}else{
 			sender.sendMessage(ChatColor.RED + "당신은 권한이 없습니다.");
 			return true;
 		}
-
+		return true;
 	 }
 }
