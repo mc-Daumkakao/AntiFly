5a9f29784ea4e421b67eef927d474b8ce72e73c3
mc-Daumkakao <antimbgh@naver.com>
2015-02-18 12:25:00 +0900

    으아악 일반유저 접근제한에서 변동없음

--- src/com/snshmedev/daumkakao/AntiFly/AntiFlyCommand.java
+++ src/com/snshmedev/daumkakao/AntiFly/AntiFlyCommand.java
@@ -1,9 +1,5 @@
 package com.snshmedev.daumkakao.AntiFly;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
 public class AntiFlyCommand {
 
 
