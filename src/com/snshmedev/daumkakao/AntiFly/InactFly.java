c71e32374a7f40668dce54eda20a35b55e69147c
mc-Daumkakao <antimbgh@naver.com>
2015-02-19 21:15:49 +0900

    이벤트 일부 구현

--- src/com/snshmedev/daumkakao/AntiFly/InactFly.java
+++ src/com/snshmedev/daumkakao/AntiFly/InactFly.java
@@ -1,7 +1,17 @@
 package com.snshmedev.daumkakao.AntiFly;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 public class InactFly implements Listener{
 	
+	public void playertpevent(PlayerChangedWorldEvent e){
+		Player p=e.getPlayer();
+		if(!p.hasPermission("essentials.fly")){
+			Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "fly " + p.getName() + " off");
+			Bukkit.broadcastMessage("fly 해제");
+		}
+	}
 }
