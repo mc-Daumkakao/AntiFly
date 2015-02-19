782420057affb141a0abcd8a83205b78684951cd
mc-Daumkakao <antimbgh@naver.com>
2015-02-19 21:35:23 +0900

    InactFly제거, 메인클래스에 통합

--- src/com/snshmedev/daumkakao/AntiFly/AntiFly.java
+++ src/com/snshmedev/daumkakao/AntiFly/AntiFly.java
@@ -1,9 +1,13 @@
 package com.snshmedev.daumkakao.AntiFly;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class AntiFly extends JavaPlugin{
+public class AntiFly extends JavaPlugin implements Listener{
 		public void onEnable(){
 			getLogger().info("AntiFly 실행");
 			getLogger().info("Spigot 1.7.9 1543기반");
@@ -15,6 +19,7 @@ public class AntiFly extends JavaPlugin{
 			saveConfig();
 			//커맨드 로드
 			getCommand("af").setExecutor(new AntiFlyCommand());
+			Bukkit.getPluginManager().registerEvents(this, this);
 		}
 
 		public void onDisable(){
@@ -22,4 +27,13 @@ public class AntiFly extends JavaPlugin{
 			getLogger().info("AntiFly 비활성화");
 			Bukkit.broadcastMessage("AntiFly플러그인 비활성화");
 		}
+
+	 @EventHandler
+	 public void playertpevent(PlayerChangedWorldEvent e){
+		Player p=e.getPlayer();
+		if(!p.hasPermission("essentials.fly")){
+			Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "fly " + p.getName() + " off");
+			Bukkit.broadcastMessage(p.getName() + " fly 해제");
+		}
+	}
 }
